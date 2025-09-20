c9fa6ca5684dd8af456597d83ebb981cbc8915c6
codetologist <codetologist@gmail.com>
2025-09-20 09:13:19 -0400

    missing commits from previous commit

--- script.py
+++ script.py
@@ -1,49 +1,64 @@
-import whois #python-whois
+import whois as python_whois #python-whois
 import time
 import sys
 import os
 from pathlib import Path
+import socket
+
+from utils.utils import isValidDomain, fileExtensionIsSupported
 
 #Usage: python script.py example.com example.org file.txt
+def extractValidDomainsFromUserInput(listOfDomainsOrFiles):
+    #iterate over user input, if a valid file is found, parse it and append found valid domains to a list of domains.
+    #TODO: handle invalid file names, unrecognized file extensions, remove duplicates
+    #TODO: Throw an error? warning? Logging?
 
-def isValidDomain(domain):
-    #TODO: implement a proper domain validation
-    #basic validation, can be improved
-    if "." in domain and " " not in domain:
-        return True
-    else: 
-        print(f"Invalid domain: {domain}")
-    return False
-
-if __name__ == "__main__":
-    scriptFileName = os.path.basename(__file__) #same as using sys.argv[0]
-    domains = [domain for domain in sys.argv if domain != "" and domain != scriptFileName] #exclude script name and empty strings
-    
-    #iterate over domains, if a string ends with .txt append it to a list of files
-    txt_files = []
-    for domain in domains:
-        if domain.endswith(".txt"):
-            txt_files.append(domain)
+    listOfValidDomains = []
+    supported_files = []
+    for domainOrFile in listOfDomainsOrFiles:
+        if fileExtensionIsSupported(domainOrFile):
+            supported_files.append(domainOrFile)
+        elif isValidDomain(domainOrFile):
+            listOfValidDomains.append(domainOrFile)
     
     #verify it exists, read it line by line and add non empty lines to the list of  domains
-    for txt_file in txt_files:
-        if Path(txt_file).is_file():
-            with open(txt_file) as f:
+    for supported_file in supported_files:
+        if Path(supported_file).is_file():
+            with open(supported_file) as f:
                 file_domains = [line.strip() for line in f if line.strip() != "" and isValidDomain(line.strip())]
-                domains.extend(file_domains)
-            domains.remove(txt_file) #remove the file name from the list of domains
+                listOfValidDomains.extend(file_domains)
         else:
-            print(f"File {txt_file} does not exist.")
-            domains.remove(txt_file) #remove the non-existent file name from the list of domains
+            print(f"File {supported_file} does not exist.") 
+    return listOfValidDomains
 
-    if len(domains) > 0: 
+
+def assingment1 ():
+    scriptFileName = sys.argv[0]
+    userInputList = sys.argv[1:]
+
+    if len(userInputList) == 0:
+        print(f"Usage: python {scriptFileName} example.com example.org file.txt")
+        return
+
+    listOfValidDomains = extractValidDomainsFromUserInput(userInputList)
+    
+    if len(listOfValidDomains) > 0: 
         delay_seconds = 5
-        for domain in domains:
+        # Set the default socket timeout to 10 seconds
+        socket.setdefaulttimeout(10)#TODO: this appears to not work as expected
+        for domain in listOfValidDomains:
             try:
-                w = whois.whois(domain)
+                w = python_whois.whois(domain)
                 print(f"{domain}: {w.registrar}")
+            except socket.timeout:
+                print("Operation timed out when querying {domain}: {e}")
             except Exception as e:
                 print(f"Error querying {domain}: {e}")
-            time.sleep(delay_seconds)
+            time.sleep(delay_seconds) #TODO: if last element, do not sleep
+        # You can reset the default timeout to None (no timeout) or another value later if needed
+        socket.setdefaulttimeout(None)
     else:
-        print("Please provide a list of domains as a command-line argument and/or a file names ending in .txt.")
+        print("Please provide a list of valid domains as a command-line argument and/or a file names ending in .txt.")
+
+if __name__ == "__main__":
+    assingment1()
